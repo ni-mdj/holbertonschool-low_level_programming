624297fb6eaa7fd2f6ecfe15b628b7de559105e8
ni-mdj <9855@holbertonstudents.com>
2024-10-21 13:46:43 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,15 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
+ *
+ * Description: Prints a quote using the puts function
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
+	 puts("\"Programming is like building a multilingual puzzle");
 
-  return (0); 
- }   
+	return (0);
+}
