cdc840629e37195eec3c75bb9dd85b99cdefe43c
ni-mdj <9855@holbertonstudents.com>
2024-10-24 23:08:34 +0000

    alphabet2

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,24 +7,24 @@
  * Return: Always 0 (Success)
  */
 
-int main(voi)
+int main(void)
 {
 	char alphabet = 'a';
 	char alphabet2 = 'A';
 
-	while(alphabet <== 'z')
+	while (alphabet <= 'z')
 	{
 		putchar(alphabet);
 		alphabet++;
 	}
 
-	while(alphabet2 <= 'Z')
+	while (alphabet2 <= 'Z')
 	{
 		putchar(alphabet2);
 		alphabet2++;
 	}
 
-	putchar('n\');
+	putchar('\n');
 
 	return (0);
 }
