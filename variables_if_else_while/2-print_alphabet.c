1dbe5b955c329c8843242a98d95e903e87669f3c
ni-mdj <9855@holbertonstudents.com>
2024-10-24 14:00:50 +0000

    print

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+int main(void);
+{
+	char alphabet;
+
+	for (char alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	putchar(alphabet);
+}
+	putchar('\n');
+
+return (0);
+}
