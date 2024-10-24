04515292c927f69b788f881c8200baf0ad350ea9
ni-mdj <9855@holbertonstudents.com>
2024-10-24 23:04:43 +0000

    alphabet2

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Descritpion: Prints lowercase and uppercase alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(voi)
+{
+	char alphabet = 'a';
+	char alphabet2 = 'A';
+
+	while(alphabet <== 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+
+	while(alphabet2 <= 'Z')
+	{
+		putchar(alphabet2);
+		alphabet2++;
+	}
+
+	putchar('n\');
+
+	return (0);
+}
