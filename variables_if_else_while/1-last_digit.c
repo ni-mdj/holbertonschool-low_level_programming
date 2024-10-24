96c3aae6d3a30a78e3dc87edcfefb2b7648437c0
ni-mdj <9855@holbertonstudents.com>
2024-10-24 11:55:04 +0000

    1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ int main(void)
 	else if (last_digit < 6 && last_digit != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6", n, last_digit);
-		printf("and not 0\n");
+		printf(" and not 0\n");
 	}
 	return (0);
 }
