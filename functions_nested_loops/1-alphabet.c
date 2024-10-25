5ba68ef571365f5cdf27624e8ed8d1f16e24527a
ni-mdj <9855@holbertonstudents.com>
2024-10-25 14:56:56 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in uppercase
+ *
+ * Description: This program prints the alphabet in lowercase,
+ * followed by a new line. Only _putchar can be used.
+ * Retun: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	chat letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
