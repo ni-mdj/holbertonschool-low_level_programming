7a6d57731966dbf3b494e60fd8512986c1f098fc
ni_mdj <9855@holbertonstudents.com>
2024-10-28 09:06:36 +0000

    isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
+ * main - hecks for uppercase character
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char c;
+char c;
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+c = 'A';
+printf("%c: %d\n", c, _isupper(c));
+c = 'a';
+printf("%c: %d\n", c, _isupper(c));
+return (0);
 }
