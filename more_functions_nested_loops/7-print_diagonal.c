622776b5dc1f83d7d8623e5500905484d55613ab
ni-mdj <9855@holbertonstudents.com>
2024-10-29 12:40:19 +0000

    printdiagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that draws a diagonal line on the terminal
+ * @n : riba
+ * Return: Always 0
+ */
+
+void print_diagonal(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
