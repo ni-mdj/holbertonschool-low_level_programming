5733e605992d8129f70c0600eb8a6ceb71b7c078
ni-mdj <9855@holbertonstudents.com>
2024-10-29 15:09:54 +0000

    printdiagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,9 +7,10 @@
  */
 
 void print_diagonal(int n)
-int i
-int j
 {
+int i;
+int j;
+
 if (n <= 0)
 {
 _putchar('\n');
@@ -17,8 +18,10 @@ _putchar('\n');
 for (i = 0; i < n; i++)
 {
 for (j = 0; j < i; j++)
-}
+{
 _putchar(' ');
 }
 _putchar(92);
 _putchar('\n');
+}
+}
