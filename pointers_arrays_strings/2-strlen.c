947b58b21e53c56d31ab0b60a43b2dbe961d5a6f
ni-mdj <9855@holbertonstudents.com>
2024-10-30 14:42:04 +0000

    strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _strlen - returns the length of a string
+ * @s: the length of a string
+ *
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+int i = 0;
+
+while (*s != '\0')
+{
+i++;
+s++;
+}
+return (i);
+}
