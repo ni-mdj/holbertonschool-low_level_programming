f60018be319de37131346896f2beaf2bddef73c1
ni-mdj <9855@holbertonstudents.com>
2024-11-03 21:58:17 +0000

    cat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - concatene n caractÃres
+ * @dest: string destination
+ * @src: string source
+ * @n: numbers max caracters
+ *
+ * return: pointeur vers la chaine resultante dest
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+
+int dest_i = 0;
+int i = 0;
+
+while (dest[dest_i] != '\0')
+{
+dest_i++;
+}
+while (i < n && src[i] != '\0')
+{
+dest[dest_i + i] = src[i];
+i++;
+}
+if (i < n)
+{
+dest[dest_i + i] = '\0';
+}
+
+return (dest);
+}
