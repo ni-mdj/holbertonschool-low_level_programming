59e617c60d553b389172d572e02c58cc715883a5
ni-mdj <9855@holbertonstudents.com>
2024-11-08 08:35:08 +0000

    g

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,23 +2,23 @@
 #include "main.h"
 
 /**
- * _strchr - Locates a character in a string
+ * *_strchr - Locates a character in a string
  * @s: Pointer to the string
  * @c: The character to locate
  *
- * Return: a pointer to the first occurrence of the character c in the string s
+ * Return: a pointer to the first occurrence of the character c in the strings
  */
 
- char *_strchr(char *s, char c)
- {
- while (*s != '\0')
- {
- if (*s == c)
-return s;
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
 s++;
- }
- if (*s == c)
- return s;
+}
+if (*s == c)
+return (s);
 
-return NULL;
- }
+return (NULL);
+}
