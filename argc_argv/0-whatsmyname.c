3954ad73206ecdf86aef47fa754f9d701926b8f7
ni-mdj <9855@holbertonstudents.com>
2024-11-08 07:47:50 +0000

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+ * main - Prints the name of the program followed by its arguments
+ * @argc: Number of arguments
+ * @argv: Array of argument strings
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
