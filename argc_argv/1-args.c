9c5c1d609f802be5dd15003a4e9af28ff50938ec
ni-mdj <9855@holbertonstudents.com>
2024-11-08 07:52:34 +0000

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed into it
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+
+printf("%d\n", argc - 1);
+return (0);
+}
