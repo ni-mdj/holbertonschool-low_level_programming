74e8118b7ce798284f80374331f1e64c38fbddd2
ni-mdj <9855@holbertonstudents.com>
2024-11-14 21:18:16 +0100

    ffffff

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,39 +1,54 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * str_concat - function that concatenates two strings
- * @s1: the first string
- * @s2: the second string
+ * str_concat - multiply numbers
+ * @s1 : variable for the first number
+ * @s2 : variable for the seconde number
  *
- * Return: pointer to the concatenated string on success,
- *         NULL if memory allocation fails.
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
  */
+
 char *str_concat(char *s1, char *s2)
 {
-char *s;
+char *concat;
 int len1;
 int len2;
-int pizza;
-int pasta;
+int i;
+int j;
 
 len1 = 0;
 len2 = 0;
 if (s1 == NULL)
+{
 s1 = "";
+}
 if (s2 == NULL)
+{
 s2 = "";
-while(s1[len1])
+}
+while (s1[len1] != '\0')
+{
 len1++;
-while(s2[len2])
+}
+while (s2[len2] != '\0')
+{
 len2++;
-s = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-if(s == NULL)
+}
+concat = malloc(len1 + len2 + 1);
+if (concat == NULL)
+{
 return (NULL);
-for (pizza = 0; pizza < len1; pizza++)
-s[pizza] = s1[pizza];
-for (pasta = 0; pasta < len2; pasta++, pizza++)
-s[pizza] = s2[pasta];
-s[pizza] = '\0';
-return (s);
+}
+for (i = 0; i < len1; i++)
+{
+concat[i] = s1[i];
+}
+for (j = 0; j < len2; j++)
+{
+concat[i + j] = s2[j];
+}
+concat[i + j] = '\0';
+return (concat);
 }
