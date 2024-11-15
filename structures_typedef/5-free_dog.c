38841fcd4dfef3cda10bbde5567d748e0761adcf
ni-mdj <9855@holbertonstudents.com>
2024-11-15 20:57:13 +0000

    q

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,18 +1,21 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * free_dog - Frees memory allocated for a dog
- * @d: Pointer to dog_t struct to free
+ * free_dog - multiply numbers
+ * @d : variable for the first number
  *
- * Description: This function frees the memory allocated for a dog,
- * including its name and owner strings.
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
  */
+
 void free_dog(dog_t *d)
 {
-if (d != NULL)
-
+if (d == NULL)
+return;
+if (d->name != NULL)
 free(d->name);
+if (d->owner != NULL)
 free(d->owner);
 free(d);
 }
