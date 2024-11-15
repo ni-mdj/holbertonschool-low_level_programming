077c7dd94a8e3253cd5d75a4e5e53cb31620bdda
ni-mdj <9855@holbertonstudents.com>
2024-11-15 03:42:12 +0000

    q

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,11 +9,11 @@
  *
  * Description: Longer description
  */
-typedef struct dog
+typedef struct dog;
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 } dog_t;
 
 /**
@@ -26,32 +26,33 @@ typedef struct dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *dog;
-    int len1, len2;
+dog_t *dog;
+int len1, len2;
 
-    len1 = strlen(name);
-    len2 = strlen(owner);
+len1 = strlen(name);
+len2 = strlen(owner);
 
-    dog = malloc(sizeof(dog_t));
-    if (dog == NULL)
-        return (NULL);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
 
-    dog->name = malloc(sizeof(char) * (len1 + 1));
-    if (dog->name == NULL)
-    {
-        free(dog);
-        return (NULL);
-    }
-    dog->owner = malloc(sizeof(char) * (len2 + 1));
-    if (dog->owner == NULL)
-    {
-        free(dog);
-        free(dog->name);
-        return (NULL);
-    }
-    strcpy(dog->name, name);
-    strcpy(dog->owner, owner);
-    dog->age = age;
-
-    return (dog);
+dog->name = malloc(sizeof(char) * (len1 + 1));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
 }
+dog->owner = malloc(sizeof(char) * (len2 + 1));
+if (dog->owner == NULL)
+{
+free(dog);
+free(dog->name);
+return (NULL);
+}
+strcpy(dog->name, name);
+strcpy(dog->owner, owner);
+dog->age = age;
+
+return (dog);
+}
+
