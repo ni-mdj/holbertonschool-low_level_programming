3c843f74b188b9f31ad847980490ac219bfd1975
ni-mdj <9855@holbertonstudents.com>
2024-11-15 01:47:38 +0100

    qqqqqqq

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -5,30 +5,28 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *pizza;
-unsigned int len1, len2, i, j;
+int len1;
+int len2;
+int i;
+int j;
 
+len 1 = 0;
+len 2 = 0;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-
 len1 = strlen(s1);
 len2 = strlen(s2);
-
 if (n >= len2)
 n = len2;
-
 pizza = malloc(sizeof(char) * (len1 + n + 1));
 if (pizza == NULL)
-return NULL;
-
+return (NULL);
 for (i = 0; i < len1; i++)
 pizza[i] = s1[i];
-
 for (j = 0; j < n; j++, i++)
 pizza[i] = s2[j];
-
 pizza[i] = '\0';
-
 return (pizza);
 }
