99cce1852daf06886c4b4084cee55d8700204d41
ni-mdj <9855@holbertonstudents.com>
2024-11-19 15:10:38 +0100

    s

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,18 +1,28 @@
-#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
-* main - check the code
+* sum_them_all - multiply numbers
+* @n: variable for the first number
 *
-* Return: Always 0.
+* Description : multiply rayane
+* Return: return 0
 */
-int main(void)
+
+int sum_them_all(const unsigned int n, ...)
 {
+va_list rayane;
+unsigned int i;
 int sum;
 
-sum = sum_them_all(2, 98, 1024);
-printf("%d\n", sum);
-sum = sum_them_all(4, 98, 1024, 402, -1024);
-printtf("%d\n", sum);
+if (n == 0)
+{
 return (0);
+}
+va_start(rayane, n);
+for (i = 0 ; i  < n; i++)
+{
+sum + = va_arg(rayane, int);
+}
+va_end(rayane);
+return (sum);
 }
