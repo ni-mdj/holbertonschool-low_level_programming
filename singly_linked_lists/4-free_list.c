33b2b560c541d817ad4dc19d1cb301d693686f97
ni-mdj <9855@holbertonstudents.com>
2024-12-03 16:12:24 +0100

    add 4-free_list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,9 +1,11 @@
 #include "lists.h"
 #include <stdlib.h>
+
 /**
 * free_list - Frees a list_t list.
 * @head: The head of the list to be freed.
 */
+
 void free_list(list_t *head)
 {
 	list_t *temp;
