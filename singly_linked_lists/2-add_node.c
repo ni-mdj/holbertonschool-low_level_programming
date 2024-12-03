0e6f11de2879a94791faf8ac338850ab57cabb57
ni-mdj <9855@holbertonstudents.com>
2024-12-03 15:08:54 +0100

    add end of 2-add

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
  * add_node - Adds a new node at the beginning of a list_t list.
  * @head: A pointer to the pointer to the head of the list.
- * @str: The string to be duplicated and added to the new node.
+ * @str: The string to be duplicated and added to the new node
  *
- * Return: The address of the new element, or NULL if it failed.
+ * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
