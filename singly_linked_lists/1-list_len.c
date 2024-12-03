a4ff36bd8fdb42722f69f2a15ba1573244dce9bf
ni-mdj <9855@holbertonstudents.com>
2024-12-03 14:30:04 +0100

    add end of 1-list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* list_len - unction that returns the number of elements in a linked list_t
+* @h: pointer to the head of the list
+*
+* Return: return i
+**/
+size_t list_len(const list_t *h);
+{
+size_t i = 0;
+while (h != NULL)
+
+i++;
+h = h->next;
+return (i);
+}
