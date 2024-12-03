35cd71bc2299d31e5952f91ad2ddf75a967d779d
ni-mdj <9855@holbertonstudents.com>
2024-12-03 14:22:18 +0100

    add finaly readme.md

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
 #ifndef __LISTS_H__
 #define __LISTS_H__
+
+#include <stdio.h>
+
 /**
 * struct list_s - singly linked list
 * @str: string - (malloc'ed string)
@@ -14,6 +17,7 @@ char *str;
 unsigned int len;
 struct list_s *next;
 } list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
